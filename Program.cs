c120fe3932859b439ab3171725e0d17db935dbf8
Vitor Brito <vitor.life18092004@gmail.com>
2023-03-14 12:08:23 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string original = "1 2 3 4 5";
+
+        string invertida = "";
+
+        for (int i = original.Length - 1; i >= 0; i--)
+        {
+            invertida += original[i];
+        }
+
+        Console.WriteLine(invertida);
+    }
+}
